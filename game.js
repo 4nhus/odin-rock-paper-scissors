70df6d6666b915b2be3360f82f6454d5490d9e66
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2023-09-10 14:00:25 +1000

    Add playRound() function to game.js

--- game.js
+++ game.js
@@ -1,12 +1,53 @@
 function getComputerChoice() {
     switch (Math.floor(Math.random() * 3) + 1) {
-        case (1):
+        case 1:
             return "rock";
-        case (2):
+        case 2:
             return "paper";
-        case (3):
+        case 3:
             return "scissors";
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = String(playerSelection).toLowerCase();
+    computerSelection = String(computerSelection).toLowerCase();
+
+    switch (playerSelection) {
+        case "rock":
+            switch (computerSelection) {
+                case "rock":
+                    return "Tie!";
+                case "paper":
+                    return "You Lose! Paper beats Rock";
+                case "scissors":
+                    return "You Win! Rock beats Scissors";
+                default:
+                    return "Invalid choice for player!";
+            }
+        case "paper":
+            switch (computerSelection) {
+                case "rock":
+                    return "You Win! Paper beats Rock";
+                case "paper":
+                    return "Tie!";
+                case "scissors":
+                    return "You Lose! Scissors beats Paper";
+                default:
+                    return "Invalid choice for player!";
+            }
+        case "scissors":
+            switch (computerSelection) {
+                case "rock":
+                    return "You Lose! Rock beats Scissors";
+                case "paper":
+                    return "You Win! Scissors beats Paper";
+                case "scissors":
+                    return "Tie!";
+                default:
+                    return "Invalid choice for player!";
+            }
+        default:
+            return "Invalid choice for player!";
+    }
+}
